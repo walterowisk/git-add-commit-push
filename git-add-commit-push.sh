30574ca11571f57b0de26f96e5beaba2e24cb6e3
Walter J. Silva <walter4web@gmail.com>
2023-09-14 11:58:33 -0300

    :recycle: refactor: add new functions && variables

--- git-add-commit-push.sh
+++ git-add-commit-push.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
+# ++++ STABLE VERSION ++++
+
 # Verifica se o usuário forneceu o nome da branch e a mensagem de commit
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <branch_name> <commit_message>"
+if [ "$#" -lt 2 ]; then
+  echo "Dica: Use o script no seguinte formato => $0 <branch_name> <commit_message>"
   exit 1
 fi
 
 branch_name="$1"
-commit_message="$2"
-
-# Verifica se existe alguma alteração para ser comitada
-check_changes() {
-  if [ -z "$(git status --porcelain)" ]; then
-    echo "Não há alterações para commit."
-    exit 0
-  fi
-}
-
-# Adiciona alterações, faz commit e push
-commit_and_push() {
-  git add .
-  git commit -m "$commit_message"
-  git push origin "$branch_name"
-}
-
-# Início das verificações do script
-echo "Executando script para automatizar ações do Git..."
+commit_message="${2:-Commit automático}"
 
 # Verifica se o repositório atual é um repositório git
 if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
@@ -33,13 +17,40 @@ if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   exit 1
 fi
 
+# Verifica se o repositório remoto existe
+if ! git ls-remote --exit-code origin; then
+  echo "Erro: Repositório remoto 'origin' não encontrado."
+  exit 1
+fi
+
 # Muda para a branch especificada
-git checkout "$branch_name"
+git checkout "$branch_name" || exit 1
 
-# Verifica se existem alterações
-check_changes
+# Exibe a lista de arquivos modificados
+git status --short
 
-# Executa commit e push
-commit_and_push
+# Solicita ao usuário que escolha os arquivos a serem commitados de acordo com a sequência exibida
+read -p "Digite os números dos arquivos que deseja commitar (separados por espaços): " file_indices
 
-echo "Mudanças confirmadas e enviadas com sucesso para a branch $branch_name."
+# Confirma as alterações selecionadas
+if [ -n "$file_indices" ]; then
+  selected_files=()
+  while read -ra indices; do
+    for index in "${indices[@]}"; do
+      selected_files+=($(git status --short | sed -n "${index}p" | awk '{print $2}'))
+    done
+  done <<< "$file_indices"
+
+  if [ "${#selected_files[@]}" -gt 0 ]; then
+    git add "${selected_files[@]}"
+    git commit -m "$commit_message"
+    git push origin "$branch_name"
+    echo
+    echo "✨️As mudanças foram confirmadas e enviadas com sucesso para a branch $branch_name!!✨️"
+    echo
+  else
+    echo "Nenhum arquivo selecionado para commit. Operação cancelada."
+  fi
+else
+  echo "Nenhum arquivo selecionado para commit. Operação cancelada."
+fi
